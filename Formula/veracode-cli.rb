55c0c73fa69ee33aa5a253dc2254a1752329e6b5
shahid-veracode <tjarrett@veracode.com>
2024-07-25 19:07:54 +0000

    Brew formula update for veracode-cli version 2.28.0

--- Formula/veracode-cli.rb
+++ Formula/veracode-cli.rb
@@ -1,16 +1,16 @@
 class VeracodeCli < Formula
   desc "You use the Veracode CLI to perform various actions for testing the security of your applications."
   homepage "https://www.veracode.com"
-  version "2.14.0"  
+  version "2.28.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.14.0_macosx_arm64.tar.gz"
-      sha256 "a875aa42e4478ae645bdc0001bf0f4a1f9af064b8887d4649c759fdc40b03c70"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_macosx_arm64.tar.gz"
+      sha256 "cad9800ef4b592ea150b9b247f244340cd8b6433be68f5b54fc9c6d072f1454f"
     elsif Hardware::CPU.intel?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.14.0_macosx_x86.tar.gz"
-      sha256 "3872e68575206ffea68bc4a276211af07085657739499ca278fd90ac1d60d5e8"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_macosx_x86.tar.gz"
+      sha256 "0557f9353e18f80f32e7e58d202e75efb7d6e0ec83325f8afa49321657216211"
     end
 
     def install
@@ -20,21 +20,21 @@ class VeracodeCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.14.0_linux_x86.tar.gz"
-      sha256 "9db91ea4fdfa4ae3ff4f39d6e417af14ec7b96485354b20f335122142eeb8e0c"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_linux_x86.tar.gz"
+      sha256 "dd067965fc8b59f58f6e6aa98fbeda464274f5b9abeefb0f758c0090fd774dd3"
 
       def install
         bin.install "veracode"
       end
-    end
+    fi
     if Hardware::CPU.intel?
-      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.14.0_linux_x86.tar.gz"
-      sha256 "9db91ea4fdfa4ae3ff4f39d6e417af14ec7b96485354b20f335122142eeb8e0c"
+      url "https://tools.veracode.com/veracode-cli/veracode-cli_2.28.0_linux_x86.tar.gz"
+      sha256 "dd067965fc8b59f58f6e6aa98fbeda464274f5b9abeefb0f758c0090fd774dd3"
 
       def install
         bin.install "veracode"
       end
-    end
+    fi
   end
 
   test do
